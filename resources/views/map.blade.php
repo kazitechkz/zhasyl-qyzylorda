a82e02502158886bc5f819421d3245e399cc564f
idlkz <idlkz2019@gmail.com>
2025-04-29 12:27:01 +0500

    commit changed map city

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,4 +1,4 @@
 @extends("layouts.front.front-layout")
 @section("main")
-    <iframe src="https://app.zhasylshym.kz/" style="width: 100%;min-height: 100vh"></iframe>
+    <iframe src="https://map.zhasylkyz.kz/" style="width: 100%;min-height: 100vh"></iframe>
 @endsection
